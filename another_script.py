32dd7b25c8f2ea3cdb404ca3376731cfe5bdbb2f
gajjarsaurav <41625708+gajjarsaurav@users.noreply.github.com>
2018-07-26 01:55:25 +0530

    Update another_script.py

--- another_script.py
+++ another_script.py
@@ -2,3 +2,5 @@ import epicthing
 
 epicthing.epicthing()   # Note here that it is not printing the 'if' statement just because it contains __name__ == '__main__'
 #   if it would not've been there, then it would've printed the next print statement also
+#   Note: Whenever we import any module it going to run thriugh that module and perform all the executable tasks by default
+
