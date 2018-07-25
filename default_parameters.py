df5cac3e0d23dc275a4ea0e6c8a50b9986327f02
gajjarsaurav <41625708+gajjarsaurav@users.noreply.github.com>
2018-07-25 19:40:34 +0530

    Add files via upload

---
+++ default_parameters.py
@@ -0,0 +1,12 @@
+def simple(num1, num2):
+    pass;
+
+def simple(num1, num2=5):
+    print(num1, num2)
+
+def basic_window(width, height, font='TNR', bgc='w', scrollBar='true'):
+    print(width, height, font, bgc)
+
+
+basic_window(232, 546)
+simple(6)
