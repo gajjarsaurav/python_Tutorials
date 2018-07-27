842cc1f189036abd7065521f4fbc2e1065faf64b
gajjarsaurav <41625708+gajjarsaurav@users.noreply.github.com>
2018-07-28 00:19:54 +0530

    Add files via upload

---
+++ tuple_and_lists.py
@@ -0,0 +1,10 @@
+x = 3,4,6,7,5   # python interpreter recognises this as a tuple as it has no '[]' brackets
+
+y = (3,4,5,7,8,7)   #   This is also a tuple
+#    basically tuple are a group of data which are immutable or can't be manipulated or changed
+
+#   and here's a what we call List
+z = [2,244,6,5,7,8,9]
+
+#   accessing the values of a tuple
+print(y[3], x[2])
