b19e469a30883fc7acd47091aab4f1cb4bfbf570
gajjarsaurav <41625708+gajjarsaurav@users.noreply.github.com>
2018-07-31 23:00:34 +0530

    Add files via upload

---
+++ builtInFunctions.py
@@ -0,0 +1,24 @@
+# using absolute value
+exNum1 = -34
+exNum2 = 34
+print(abs(exNum1))
+if exNum1 is exNum2:
+    print('these are the same')
+else:
+    print('they are not the same')
+
+# for finding max and min no in a given list
+exList = [3,5,7,8,3,6,7,3]
+print(max(exList))
+print(min(exList))
+
+# for rounding off
+x = 4.578
+print(round(x))
+import math
+print(math.floor(x))
+print(math.ceil(x))
+
+# type conversion in python
+no = '38'
+print(float(no))
